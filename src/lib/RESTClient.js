e517e36f3645f461eef8ac0db9874fbea0ede09e
Terazus <batistadominique@hotmail.com>
2023-03-01 10:03:12 -0800

    updated API url

--- src/lib/RESTClient.js
+++ src/lib/RESTClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "http://127.0.0.1:5000/api" // "https://pretox.isa-tools.org/api" //
+const BASE_URL = "https://pretox.isa-tools.org/api" // "http://127.0.0.1:5000/api"
 const HEADERS = { "Content-Type": "application/json", "Accept": "application/json" }
 
 
