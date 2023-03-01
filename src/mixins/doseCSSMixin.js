b8da22d67f584d750986412dcbf2be50293d90a3
Terazus <batistadominique@hotmail.com>
2023-03-01 03:15:08 -0800

    removed console logs

--- src/mixins/doseCSSMixin.js
+++ src/mixins/doseCSSMixin.js
@@ -7,7 +7,6 @@ export default {
             return 'grey--text'
         },
         doseColor(dose) {
-            console.log(dose)
             if (dose === 'LOW' || dose === 0 || dose === 'BMD10') return 'green'
             else if (dose === 'HIGH' || dose === 2 || dose === '10mg/L') return 'red'
             else if (dose === 'MEDIUM' || dose === 1 || dose === 'BMD25') return 'primary'
