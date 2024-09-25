cacf2e064a3a749246ffbeb93841b91588c173cf
Milo Thurston <knirirr@gmail.com>
2024-09-25 15:30:30 +0100

    Initial package upgrade.

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': '@vue/vue3-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
